c45e05b716b5acc23bb4844fa92fbf1347218be6
Kjetil Mehl <kjetil.mehl@uninett.no>
2012-06-28 16:06:53 +0200

    Added module for polling running tests.

--- perlui/lib/DNSCheckWeb/DB.pm
+++ perlui/lib/DNSCheckWeb/DB.pm
@@ -13,10 +13,7 @@ use constant TYPE_MYSQL => "mysql";
 my $dbo;
 
 sub new {
-	my $class = shift;
-
-	# We expect some database info.
-	my $db_info = shift;
+	my ($class, $db_info) = @_;
 	# Create empty object, will be filled.
 	my $self = {};
 
@@ -67,17 +64,81 @@ sub new {
 	return $self;
 }
 
-# Fire of a new check, return id.
+# Fires of a new check
 sub start_check {
-	my $self = shift;
-	my $domain = shift;
+	my ($self, $domain, $source, $source_data) = @_;
+
+	# Lets see if we have source id
+	my $source_id = $self->get_source_id($source);
 
 	my $dbh = $self->{dbh};
 	my $query = $dbh->prepare(q{
-		INSERT INTO queue (domain, priority) VALUES (?, 10)})
+		INSERT INTO
+		queue (domain, priority, source_id, source_data, fake_parent_glue)
+		VALUES (?, 10, ?, ?, ?)})
 		or die "Could not prepare statement";
-	$query->execute($domain);
-	#$query = "INSERT INTO queue (domain, priority, source_id, source_data, fake_parent_glue) VALUES ('" . DatabasePackage::escape($domain) . "', 10, $id, '" . DatabasePackage::escape($sourceData) . "', '" . DatabasePackage::escape($sourceData) . "')";
+	$query->execute($domain, $source_id, '', $source_data);
+}
+
+# Returns id for the given source, or creates a new one.
+sub get_source_id {
+	my($self, $source) = @_;
+
+	my $dbh = $self->{dbh};
+	my $query = $dbh->prepare(q{
+		SELECT id FROM source WHERE name = ?})
+		or die "Could not prepare statement";
+	$query->execute($source);
+
+	my $result = $query->fetchrow_arrayref;
+	# Insert new source
+	if(!defined($result)) {
+		$query = $dbh->prepare(q{ INSERT INTO source (name) VALUES (?) });
+		$query->execute($source) or die "Database query failed";
+
+		return $dbh->last_insert_id(undef, undef, qw(source id));
+	} else {
+		# Dereference and return result
+		return @$result[0];
+	}
+}
+
+# Checks the queue for a running result
+sub get_running_result {
+	my ($self, $domain, $source, $source_data) = @_;
+
+	$source_data = '';
+
+	my $dbh = $self->{dbh};
+	my $query = $dbh->prepare(q{
+		(SELECT
+			NULL AS id, NULL AS time, 'NO' AS finished,
+			source_data AS source_data
+		FROM queue
+			INNER JOIN
+				source ON source.id = queue.source_id
+				AND source.name = ?
+		WHERE
+			queue.domain = ? AND queue.source_data = ?)
+		UNION
+		(SELECT
+			tests.id AS id, date_part('epoch', tests.started) AS TIME,
+			CASE tests.finished WHEN NULL THEN 'NO' ELSE 'YES' END AS finished,
+			source_data AS source_data
+		FROM tests
+			INNER JOIN
+				source ON source.id = tests.source_id AND source.name = ?
+		WHERE
+			tests.domain = ? and tests.source_data = ?
+			AND (tests.finished = null
+			OR (date_part('epoch', now()) - date_part('epoch',
+			tests.finished)) < 300))
+	}) or die "Prepare statement failed";
+
+	$query->execute($source, $domain, $source_data, $source, $domain,
+	$source_data) or die "Could not execute query";
+
+	return $query->fetchall_arrayref;
 }
 
 # Fetches the version of DNSChecker that we are running.
