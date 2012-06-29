b5141222d86d9d3f3a81e16890cc8602fb01b6d7
Kjetil Mehl <kjetil.mehl@uninett.no>
2012-06-29 12:16:52 +0200

    Index is now just a wrapper and should direct its queries to the pollRequest script. Poll request should check the actual state of the running tests (and return this).

--- perlui/web/index.pl
+++ perlui/web/index.pl
@@ -9,54 +9,13 @@ use CGI;
 # Testing
 use Data::Dumper;
 
-use constant TYPES => {
-	standard => "webgui",
-	undelegated => "webgui-undelegated"
-};
-
 my $cgi = CGI->new();
-my $host = $cgi->param("host");
-my $source = TYPES->{$cgi->param("test")};
-my $source_data = $cgi->param("parameters");
-
 my $dnscheck = DNSCheckWeb->new();
 my $dbo = $dnscheck->get_dbo();
 
-# Variables for giving feedback
-my $status;
-my $test_id;
-
-# Someone sent a query, no id hence insert in DB.
-if(defined($host) && defined($source)) {
-
-
-	my $running = $dbo->get_running_result($host, $source, $source_data);
-
-
-	if(@$running eq 0) {
-		# No tests running, fire of new test.
-		$dbo->start_check($host, $source, $source_data);
-		$status = "started new run";
-	}
-
-	if($running->[0][2] eq 'NO') {
-		$status = "it's not finished...";
-	}
-	$test_id = $running->[0][0];
-}
-
-# Lets redirect when we get results.
-if(defined($test_id) && $test_id > 0) {
-	print "Location: pollResult.pl?id=$test_id\n\n";
-	exit;
-}
-
 # Render result
 $dnscheck->render('index.tpl', {
-	host => $host,
-	version => $dbo->get_version(),
-	status => $status,
-	id => $test_id,
+	version => $dbo->get_version()
 });
 
 1;
--- perlui/web/pollResult.pl
+++ perlui/web/pollResult.pl
@@ -13,33 +13,54 @@ use warnings;
 use DNSCheckWeb;
 use CGI;
 use JSON;
-
 # Testing
 use Data::Dumper;
 
+# Constants for the "legal" types
+use constant TYPES => {
+	standard => "webgui",
+	undelegated => "webgui-undelegated"
+};
+# Some important objects
 my $cgi = new CGI;
-my $test_id = $cgi->param("id");
-my $locale = $cgi->param("locale");
-
-# Load config
 my $dnscheck = DNSCheckWeb->new();
 my $dbo = $dnscheck->get_dbo();
 
-my $results;
+# Fetch parameters
+my $host = $cgi->param("host");
+my $source = TYPES->{$cgi->param("test")};
+my $source_data = $cgi->param("parameters");
+my $locale = 'en';
 
-# Feed back result to browser
-print $dnscheck->json_headers();
+# Variables for giving feedback
+my $test_id; # If test is finished, this will be set
+my $href_results = {}; # Final json-string containing status and results
 
-# Fetch all results on the given test_id 
-if(defined($test_id) && $test_id > 0 && defined($locale)) {
-	$results = $dbo->get_test_results($test_id, $locale);
+# Received host name, check for running tests
+if(defined($host) && defined($source)) {
+	my $running = $dbo->get_running_result($host, $source, $source_data);
 
-	my $json = encode_json $results;
-	$json = "{\"results\": ".$json."}";
-
-	print $json;
+	if(@$running eq 0) {
+		# No tests running, fire of new test.
+		$dbo->start_check($host, $source, $source_data);
+		$href_results->{status} = "started new run";
+	} elsif($running->[0][2] eq 'NO') {
+		# Test for domain is running, but not finished
+		$href_results->{status} = "Not finished yet..";
+	} else {
+		# Finished test, set test_id
+		$test_id = $running->[0][0];
+	}
 }
 
-exit;
+# Fetch the results given that we got a test id
+if(defined($test_id) && $test_id > 0 && defined($locale)) {
+	my $json =  $dbo->get_test_results($test_id, $locale);
+	$href_results->{tests} = $json;
+}
+# Feed result back to browser
+print $dnscheck->json_headers();
+print encode_json $href_results;
 
+exit;
 1;
