2a821eaea1748e1f7b5a71bae89234bc3c8866fb
Kjetil Mehl <kjetil.mehl@uninett.no>
2012-07-16 12:58:22 +0200

    A better path.

--- perlui/startup.pl
+++ perlui/startup.pl
@@ -2,7 +2,7 @@
 #  Absolute path to library. Apache should point to this to load the
 #  needed modules.
 #
-use lib  qw( /var/www/perlui/lib/ );
+use lib  qw( /var/www/dnscheck/lib/ );
 
 #
 #  This script had no errors (?!)
